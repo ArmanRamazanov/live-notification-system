d5d9d612337ab6fd0b30b9ff32e750b50978a3f0
ArmanRamazanov <arman.ramazanov.04@mail.ru>
2026-06-18 16:24:55 +0900

    updated

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,12 +1,23 @@
 import type { Request, Response } from "express";
 import { Router } from "express";
 import { verifyToken } from "../middleware/tokenVerification";
+import {
+  getNotificationsController,
+  loginController,
+  notificationController,
+  readNotificationController,
+} from "../controllers/controller";
 
 const router = Router();
 
-router.post("/login");
-router.post("/notifications", verifyToken);
-router.post("/notifications/:id/read", verifyToken);
+router.post("/login", loginController);
+router.post("/notifications", verifyToken, notificationController);
+router.patch(
+  "/notifications/:id/read",
+  verifyToken,
+  readNotificationController,
+);
+router.get("/notifications", verifyToken, getNotificationsController);
 
 router.get("/health", (req: Request, res: Response) => {
   res.json({
